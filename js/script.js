d7c830959245ca64620afde49496385ed8622377
Glitch (playlist-project-mikkail) <none>
2021-03-04 22:50:38 +0000

    😄🌴 Updated with Glitch

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let image1 = ""
+let myPlaylistSongs = ["Bands", "New Tank", "Time Today", "Patek", "Runnin", "Portland"]
+let myPlaylistArtists = ["Comethazine", "Playboi Carti", "Moneybagg Yo", "Lil Uzi Vert", "21 Savage", "Drake"]
+let songLength = ["1:38", "1:28", "2:16", "4:40", "3:15", "3:54"]
+let songImage = []
+let songLink = []
+$("body").html(image1);
